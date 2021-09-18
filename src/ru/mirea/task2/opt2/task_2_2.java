251bfcf7912107513c0c28e5974f996482fa7dd3
Mazan-ka <Mokeevskoe1>
2021-09-18 17:18:29 +0300

    para

--- src/ru/mirea/task2/opt2/task_2_2.java
+++ src/ru/mirea/task2/opt2/task_2_2.java
@@ -3,13 +3,15 @@ package ru.mirea.task2.opt2;
 public class task_2_2 {
      public static void main(String[] args) {
          Ball myach = new Ball (22.2, 44.4);
-         myach.test();
+         System.out.println(myach);
+         myach.move(2.5, 3.7);
+         System.out.println(myach);
      }
 }
 
 class Ball {
     private double x = 0.0;
-    private  double y = 0.0;
+    private double y = 0.0;
 
     public Ball (double x, double y) {
         setXY(x,y);
@@ -38,36 +40,14 @@ class Ball {
     }
 
     public void move(double xDisp, double yDisp) {
-        x =+ xDisp;
-        y =+ yDisp;
+        x += xDisp;
+        y += yDisp;
     }
 
     public String toString() {
         return "Ball @(" + x + "," + y + ")";
     }
-    public void test () {
-        testBall test = new testBall(x,y);
-        test.Cout();
-        test.testMove();
-        test.Cout();
-    }
 }
 
-class testBall extends Ball{
-    double x1;
-    double y1;
-    public testBall(double x, double y) {
-        x1 = x;
-        y1 = y;
-    }
-
-    public void Cout() {
-        System.out.println(toString());
-    }
-
-    public void testMove() {
-        move(2.5, 3.7);
-    }
-}
 
 
