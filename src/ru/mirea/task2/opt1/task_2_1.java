99ff0eb9a26338ccf57216e0752f0f494eae4613
Mazan-ka <Mokeevskoe1>
2021-09-18 15:13:08 +0300

    para

---
+++ src/ru/mirea/task2/opt1/task_2_1.java
@@ -0,0 +1,17 @@
+package ru.mirea.task2.opt1;
+
+public class task_2_1 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Shape {
+    private String typeFigure;
+    private String color;
+
+    public Shape (String typeFigure) {
+        this.typeFigure = typeFigure;
+    }
+
+}
