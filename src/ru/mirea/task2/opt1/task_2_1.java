b8d5cb183cc87b88235f28f881e7b365e441cee0
Mazan-ka <Mokeevskoe1>
2021-09-18 17:20:54 +0300

    para

--- src/ru/mirea/task2/opt1/task_2_1.java
+++ src/ru/mirea/task2/opt1/task_2_1.java
@@ -2,7 +2,8 @@ package ru.mirea.task2.opt1;
 
 public class task_2_1 {
     public static void main(String[] args) {
-
+        Shape triangle = new Shape("triangle", "red");
+        System.out.println(triangle.toString());
     }
 }
 
@@ -10,8 +11,20 @@ class Shape {
     private String typeFigure;
     private String color;
 
-    public Shape (String typeFigure) {
+    public Shape (String typeFigure, String color) {
+        setColor(color);
+        setTypeFigure(typeFigure);
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setTypeFigure(String typeFigure) {
         this.typeFigure = typeFigure;
     }
 
+    @Override
+    public String toString() {
+        return "Shape{" + "typeFigure='" + typeFigure + '\'' + ", color='" + color + '\'' + '}';
+    }
 }
