3b9e30cf81c0157d8609fefe17aeb799338e520c
Mazan-ka <Mokeevskoe1>
2021-09-18 13:57:15 +0300

    para

--- src/ru/mirea/task1/opt5/task_1_5.java
+++ src/ru/mirea/task1/opt5/task_1_5.java
@@ -1,12 +1,19 @@
 package ru.mirea.task1.opt5;
 
+import java.util.Scanner;
+
 public class task_1_5 {
-    public static int factorial(int x) {
-        int res=0;
-        if (x<0){
-            res = x*(x-1);
-            x = x-2;
+    static int factorial(int x) {
+        int res=1;
+        for (int i = x; i > 1; i--) {
+            res *= i;
         }
-        return x; //фигня
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        System.out.println(factorial(x));
     }
 }
