aff9de784f26edbcd6c09368ef620b1329f36d7e
Mazan-ka <Mokeevskoe1>
2021-12-16 14:42:36 +0300

    para

---
+++ src/ru/mirea/task27/City.java
@@ -0,0 +1,19 @@
+package ru.mirea.task27;
+
+public final class City {
+    private final String name;
+    private final String country;
+
+    public City(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
